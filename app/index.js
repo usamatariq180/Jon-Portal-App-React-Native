f5454e50a40cd424fed078e48e82187e130a81c6
Usama Tariq <100127810+usamatariq180@users.noreply.github.com>
2023-12-06 12:01:04 +0500

    first commit

---
+++ app/index.js
@@ -0,0 +1,10 @@
+// import { Link, Stack } from 'expo-router';
+import { Image, Text, View } from 'react-native';
+
+export default function Home() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
